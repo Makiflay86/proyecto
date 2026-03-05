5b9826359f04dfa775b108d366926300af712bdd
Francisco <frannur1.ir@gmail.com>
2026-03-05 13:48:47 +0100

    toqueteando y usando tailwind por 1 vez

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,21 @@
         </h2>
     </x-slot>
 
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{-- {{ __("You're logged in!") }} --}}
+                    <h1>¡Bienvenido <strong>{{ Auth::user()->name }}</strong>!</h1>
+                    <p class="text-sm">
+                        <?php
+                            echo "Hoy es: <strong>" . date("d-m-Y") . "</strong>"
+                        ?>
+                    </p>
                 </div>
             </div>
         </div>
